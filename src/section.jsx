31d8853d86a6e379e2509eb03424aed354ec8dae
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2016-03-07 14:12:18 +0100

    Refactoring

--- src/section.jsx
+++ src/section.jsx
@@ -4,24 +4,21 @@ import { Paragraph } from './paragraph.jsx';
 import { Header } from './header.jsx';
 
 export class Section extends React.Component {
-    contents() {
-        return this.props.data.contents.map(elem => {
-            switch (elem.type) {
-                case 'editor':
-                    return <Editor key="editor" data={elem}/>;
-                case 'paragraph':
-                    return <Paragraph key={elem.id} data={elem}/>;
-                case 'section':
-                    return <Section key={elem.id} data={elem}/>;
-            }
-        });
+
+    renderContent(element) {
+        switch (element.type) {
+            case 'editor':return <Editor key="editor" data={element} />;
+            case 'paragraph': return <Paragraph key={element.id} data={element} />;
+            case 'section': return <Section key={element.id} data={element} />;
+            default: console.warn('Unknown content type: ' + element.type)
+        }
     }
 
     render() {
         return (
             <section>
                 <Header title={this.props.data.heading} />
-                {this.contents()}
+                {this.props.data.contents.map(this.renderContent)}
             </section>
         );
     }

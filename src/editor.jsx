341a1cfac8ed7ea74c01e4fa5b92d3968cd39b73
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2016-03-15 13:43:53 +0100

    Editor sends paragraphs.

--- src/editor.jsx
+++ src/editor.jsx
@@ -2,12 +2,29 @@ import React from 'react';
 
 export class Editor extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: ''
+        };
+    }
+
     handleChange(event) {
         var value = event.target.value;
-        this.props.appendElement(value);
+        if (value.endsWith('\n\n')) {
+            this.props.appendContent({
+                type: 'paragraph',
+                text: value.slice(0, -2)
+            });
+            this.setState({value: ''});
+        } else {
+            this.setState({value: value});
+        }
     }
 
     render() {
-        return <textarea rows="10" onChange={this.handleChange.bind(this)}></textarea>;
+        return <textarea rows="10"
+                         onChange={this.handleChange.bind(this)}
+                         value={this.state.value} />
     }
 }

7e584f3859fd028dccf8d62e19fc4f60196d89cf
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2016-03-18 19:42:39 +0100

    Sections inserted correctly

--- src/section.jsx
+++ src/section.jsx
@@ -31,21 +31,51 @@ export class Section extends React.Component {
         var editorIndex = this.state.contents.findIndex(e => e.type === 'editor');
         var beforeContents = this.state.contents.slice(0, editorIndex);
         var afterContents = this.state.contents.slice(editorIndex);
+        var afterPars = afterContents.filter(e => e.type !== 'section');
+        var afterSections = afterContents.filter(e => e.type === 'section');
 
         var section = {
             id: this.props.getId(),
             type: 'section',
             heading: text,
-            contents: afterContents
+            contents: afterPars
         };
 
         if (level === 0) {
             this.setState({contents: beforeContents.concat(section)});
+            return true;
+        } else if (this.props.insertChildSection !== undefined) {
+            if (this.props.insertChildSection(this.state.id, [section].concat(afterSections), level - 1)) {
+                this.setState({contents: beforeContents});
+                return true;
+            } else {
+                return false;
+            }
         } else {
-            alert('Sections of this level are not supported yet.');//TODO
+            alert('You cannot insert a H1 or higher.');
             return false;
         }
-        return true;
+    }
+
+    insertChildSection(sourceId, children, level) {
+        var sectionIndex = this.state.contents.findIndex(e => e.id === sourceId);
+        var beforeContents = this.state.contents.slice(0, sectionIndex + 1);
+        var afterContents = this.state.contents.slice(sectionIndex + 1);
+
+        if (level === 0) {
+            this.setState({contents: beforeContents.concat(children, afterContents)});
+            return true;
+        } else if (this.props.insertChildSection !== undefined){
+            if (this.props.insertChildSection(this.state.id, children.concat(afterContents), level - 1)) {
+                this.setState({contents: beforeContents});
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            alert('You cannot insert a H1 or higher.');
+            return false;
+        }
     }
 
     renderContent(element) {
@@ -54,7 +84,9 @@ export class Section extends React.Component {
                                           insertParagraph={this.insertParagraph.bind(this)}
                                           insertSection={this.insertSection.bind(this)} />;
             case 'paragraph': return <Paragraph key={element.id} data={element} />;
-            case 'section': return <Section key={element.id} data={element} getId={this.props.getId} />;
+            case 'section': return <Section key={element.id} data={element}
+                                            getId={this.props.getId}
+                                            insertChildSection={this.insertChildSection.bind(this)} />;
             default: console.warn('Unknown content type: ' + element.type)
         }
     }

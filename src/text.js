3d3e54c5955ae154bbf4afff35736f8a4f6ba88d
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2016-03-27 17:39:52 +0200

    Longer text for initial testing.

---
+++ src/text.js
@@ -0,0 +1,53 @@
+export default {
+    sections : {
+        0 : {
+            id : 0,
+            heading : 'Bývalí Hunteři',
+            contents : [
+                {id : 6, text : 'Skupina hunterů ze Sunny Beach, která se už rozpadla. Konkrétně se rozpadla kvůli smrti dvou členů při honu na "upíra".'},
+                {id : 7, text : 'Částečně aktivní je pouze "Zelený mozek" (viz níže), jeden z hunterů dělá vrátného v domě, kde bydlí Jill.'}
+            ],
+            children : [1, 2, 3]
+        },
+        1 : {
+            id : 1,
+            heading : 'Zelený Mozek',
+            contents : [
+                {id : 8, text : 'aka Bill z L4D — bývalý voják, byl v Zálivu a v Bosně. Mírné psychologické trauma — v obou válkách hájil nějaké abstraktní zájmy, nebránil lidi.'},
+                {id : 9, text : 'Původně aktivně působící, když se minule moc dobře nevedlo, rozhodl se pověsit vigil na hřebík (no, to se uvidí, jak se mu to povede).'},
+                {id : 10, text : 'Vlastní nemalou zásobu diskutabilně millitary-grade zbraní (ostřelovačská puška, SMG, možná i M4).'},
+                {id : 11, text : 'S postavami má domluvenou skrýš pod mostem. Kontakt na něj má Carl.'}
+            ],
+            children : []
+        },
+        2 : {
+            id : 2,
+            heading : "Ostatní",
+            contents : [{id : 12, text : 'Původně jich bylo pět, dva to dostali při honu na upíra, jednoho později zabil Sam.'}],
+            children : [4, 5]
+        },
+        3 : {
+            id : 3,
+            heading : "Historie",
+            contents : [
+                {id : 13, text : 'Po "neúspěšném" posledním huntu se skupina rozpadla. Peterson se ostatích výrazně stranil a nakonec se z něj stal slasher. Musel ho zabít Sam.'},
+                {id : 14, text : 'Robb zmizení prošetřoval, konfrontoval Sama a dostalo se mu lekce (strávil několik měsíců v sádře). V zásadě ale pochopil, co se stalo a že Samovo řešení bylo nutné. Teď pracuje jako vrátný v domě Jill.'}
+            ],
+            children : []
+        },
+        4 : {
+            id : 4,
+            heading : "H. Robb",
+            contents : [{id : 15, text : 'žije dodnes, nechce o vigilu mluvit, dělá vrátného'}],
+            children : []
+        },
+        5 : {
+            id : 5,
+            heading : "Peterson",
+            contents : [{id : 16, text : 'vedl si deník, takový vědecký typ, stal se z něj slasher, mrtvý'}],
+            children : []
+        }
+    }
+    ,
+    editor : null
+}

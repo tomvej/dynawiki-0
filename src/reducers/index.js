082c9c0e96660eaf7ee9f63ca1cdaa041d7222a8
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2016-03-28 19:14:05 +0200

    Publish reducer prepared

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import Actions from '../actions/constants'
 import closeEditor from './closeEditor'
+import publish from './publish'
 
 const reducers = {};
 reducers[Actions.CLOSE_EDITOR] = closeEditor;
+reducers[Actions.PUBLISH] = publish;
 
 export default (state, action) => {
     let reducer = reducers[action.type];

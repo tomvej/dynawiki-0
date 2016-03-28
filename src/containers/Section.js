236b0ee3f56a14ed17eaa90e973e53f255f46744
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2016-03-28 11:21:50 +0200

    Removed semicolons from imports

--- src/containers/Section.js
+++ src/containers/Section.js
@@ -1,7 +1,7 @@
-import React, { PropTypes } from 'react';
-import { connect } from 'react-redux';
+import React, { PropTypes } from 'react'
+import { connect } from 'react-redux'
 
-import Editor from './Editor';
+import Editor from './Editor'
 
 const mapStateToProps = (state, ownProps) => ({
         section: state.sections[ownProps.id],
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import initial from './initial';
-import wikiApp from './reducers';
-import Section from './containers/Section';
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import initial from './initial'
+import wikiApp from './reducers'
+import Section from './containers/Section'
 
 render(
     <Provider store={createStore(wikiApp, initial)}>

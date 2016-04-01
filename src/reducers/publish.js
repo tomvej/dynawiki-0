f5d9d0400d1ab5fe274abd0021970547229203ce
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2016-04-01 14:00:16 +0200

    Refactoring

--- src/reducers/publish.js
+++ src/reducers/publish.js
@@ -4,42 +4,48 @@ export default (state, payload) => {
     let updateState = command => {
         state = update(state, command);
     };
-    let parent = () => state.sections[section].parent;
 
     let id = state.nextId;
-    let section = state.editor.section;
+    let sectionId = state.editor.section;
     let index = state.editor.index;
 
+    let updateContents = command => {
+        updateState({sections: {[sectionId]: {contents: command}}});
+    };
+    let updateChildren = command => {
+        updateState({sections: {[sectionId]: {children: command}}});
+    };
+    let section = () => state.sections[sectionId];
     let pushParagraph = text => {
         let paragraph = {
             id: id++,
             text: text
         };
-        updateState({sections: {[section]: {contents: {$splice: [[index, 0, paragraph]]}}}});
+        updateContents({$splice: [[index, 0, paragraph]]});
         index++;
     };
     let pushSection = (heading, level, sourceIndex = 0, children = []) => {
-        let after = state.sections[section].children.slice(sourceIndex);
-        updateState({sections: {[section]: {children: {$splice: [[sourceIndex, after.length]]}}}});
+        let after = section().children.slice(sourceIndex);
+        updateChildren({$splice: [[sourceIndex, after.length]]});
 
         if (level === 0) {
             let newId = id++;
             let newSection = {
                 id: newId,
                 heading: heading,
-                parent: section,
+                parent: sectionId,
                 contents: [],
                 children: children.concat(after)
             };
             updateState({sections: {
                 $merge : {[newId] : newSection},
-                [section] : {children: {$push: [newId]}}
+                [sectionId] : {children: {$push: [newId]}}
             }});
-            section = newId;
+            sectionId = newId;
             index = 0;
-        } else if (parent() !== null) {
-            sourceIndex = state.sections[parent()].children.indexOf(section) + 1;
-            section = parent();
+        } else if (section().parent !== null) {
+            sourceIndex = state.sections[section().parent].children.indexOf(sectionId) + 1;
+            sectionId = section().parent;
             pushSection(heading, level -1, sourceIndex, children.concat(after));
         } else {
             console.error('Trying to add another H1. Ignoring.'); //TODO move to validation
@@ -49,10 +55,10 @@ export default (state, payload) => {
     payload.forEach(element => {
         switch (element.type) {
             case 'section':
-                let orphans = state.sections[section].contents.slice(index);
-                updateState({sections: {[section]: {contents: {$splice: [[index, orphans.length]]}}}});
+                let orphans = section().contents.slice(index);
+                updateContents({$splice: [[index, orphans.length]]});
                 pushSection(element.heading, element.level);
-                updateState({sections: {[section]: {contents: {$push: orphans}}}});
+                updateContents({$push: orphans});
                 break;
             case 'paragraph':
                 pushParagraph(element.text);
@@ -65,7 +71,7 @@ export default (state, payload) => {
     updateState({
         nextId: {$set: id},
         editor: {
-            section: {$set: section},
+            section: {$set: sectionId},
             index: {$set: index}
         }
     });

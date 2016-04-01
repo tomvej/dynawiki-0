36e8685b3785c7bb7821b5132ee5577c86b5e8cd
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2016-04-01 10:20:22 +0200

    Framework for 'publish' reducer.

--- src/reducers/publish.js
+++ src/reducers/publish.js
@@ -1,3 +1,33 @@
+import update from 'react-addons-update'
+
 export default (state, payload) => {
-    return state;
+    let newState = state;
+    let updateState = command => {
+        newState = update(newState, command);
+    }
+
+    let section = state.editor.section;
+    let index = state.editor.index;
+
+    let pushParagraph = text => {
+
+    };
+    let pushSection = (heading, level) => {
+
+    };
+
+    payload.forEach(element => {
+        switch (element.type) {
+            case 'section':
+                pushSection(element.heading, element.level);
+                break;
+            case 'paragraph':
+                pushParagraph(element.text);
+                break;
+            default:
+                console.warn('Unknown content type: ' + element.type);
+        }
+    });
+
+    return newState;
 }

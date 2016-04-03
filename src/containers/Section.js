fd6358f7b1ca07fd2f44b69fdec0f6156c474ab1
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2016-04-03 17:33:33 +0200

    "Edit" link.

--- src/containers/Section.js
+++ src/containers/Section.js
@@ -16,10 +16,15 @@ const mapDispatchToProps = dispatch => ({
     }
 });
 
+const Menu = () => (
+    <a id="menu" href="" onClick={e => {e.preventDefault();}}>Edit</a>
+);
+
 const Paragraph = (id, selection, changeSelection, text) => (
     <p key={id}
        data-selected={id === selection}
        onClick={changeSelection(id)}>
+        {id === selection ? <Menu /> : null}
         {text}
     </p>
 );
@@ -31,7 +36,7 @@ const Section = ({section, editor, selection, changeSelection}) => {
     }
     return (
         <section data-selected={section.id === selection} onClick={changeSelection(section.id)}>
-            <h1>{section.heading}</h1>
+            <header><h1>{section.heading}</h1>{section.id === selection ? <Menu/> : null}</header>
             {pars}
             {section.children.map(id => <SectionContainer key={id} id={id}/>)}
         </section>

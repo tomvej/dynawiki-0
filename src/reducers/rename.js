59b5d52349dc8be83ea3ed57094b81a8d103f292
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2016-04-10 21:43:28 +0200

    Selection retained after renaming.

--- src/reducers/rename.js
+++ src/reducers/rename.js
@@ -2,5 +2,6 @@ import update from 'react-addons-update'
 
 export default (state, payload) => update(state, {
     sections: {[state.editor.section]: {heading: {$set: payload}}},
-    editor: {$set: null}
+    editor: {$set: null},
+    selection: {$set: state.editor}
 });

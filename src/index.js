600e87447a391b0b55c4d2d6017a8624c7f52c60
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2016-04-11 15:58:37 +0200

    Organize imports

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
-import initial from './initial'
 
+import initial from './initial'
 import wikiApp from './reducers'
 import Section from './containers/Section'
 

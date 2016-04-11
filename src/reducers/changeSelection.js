81d1a120744b520795e9d5998f86a7935d0a404d
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2016-04-11 10:08:12 +0200

    Removed TODO.

--- src/reducers/changeSelection.js
+++ src/reducers/changeSelection.js
@@ -1,4 +1,3 @@
 import update from 'react-addons-update'
 
-//TODO move 'editor === null check' to validation
 export default (state, payload) => (state.editor === null ? update(state, {selection: {$set: payload}}) : state);

9bfaa750fa605e4024f9df2f93978572f5d66ee3
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2016-04-15 14:01:31 +0200

    Deleting whole sections.

--- src/reducers/deleteSelection.js
+++ src/reducers/deleteSelection.js
@@ -1,9 +1,25 @@
 import update from 'react-addons-update'
 
+import children from './utils/children'
+
 export default (state, payload) => {
     let command = {selection: {$set: null}};
     if (state.selection.index === null) {
-
+        let section = state.selection.section;
+        let parent = state.sections[section].parent;
+        if (parent === null) { // cannot remove top-level section
+            Object.assign(command, { sections: {[section]: {
+                contents: {$set: []},
+                children: {$set: []}
+            }}});
+        } else {
+            let index = state.sections[parent].children.indexOf(section);
+            Object.assign(command, {sections: {
+                [section]: {$set: null},
+                [parent]: {children: {$splice: [[index, 1]]}}
+            }});
+        }
+        children(state, section).forEach(s => Object.assign(command.sections, {[s]: {$set: null}}));
     } else {
         Object.assign(command, {sections: {[state.selection.section]: {contents: {$splice: [[state.selection.index, 1]]}}}});
     }

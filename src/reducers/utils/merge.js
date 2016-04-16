18dfab1d7238b6c37f60d9b695e38ef974ec3525
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2016-04-16 09:26:59 +0200

    Refactoring

---
+++ src/reducers/utils/merge.js
@@ -0,0 +1,19 @@
+const isOtherObject = object => (!Array.isArray(object) && typeof object === 'object');
+
+const merge = (target, object) => {
+    for (const property in object) {
+        if (target.hasOwnProperty(property)) {
+            if (Array.isArray(target[property]) && Array.isArray(object[property])) {
+                [].push.apply(target[property], object[property]);
+            } else if (isOtherObject(target[property]) && isOtherObject(object[property])) {
+                merge(target[property], object[property]);
+            } else {
+                throw 'Cannot merge ' + target[property] + ' and ' + object[property];
+            }
+        } else {
+            target[property] = object[property];
+        }
+    }
+};
+
+export default merge;

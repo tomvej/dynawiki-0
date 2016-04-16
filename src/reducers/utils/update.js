091ae61218e1ca3a6c9918baaa1ae477db3da9f3
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2016-04-16 14:16:15 +0200

    Undo/redo building.

--- src/reducers/utils/update.js
+++ src/reducers/utils/update.js
@@ -17,30 +17,48 @@ const copy = target => {
 const update = (state, command) => {
     if (command.hasOwnProperty('$set')) {
         return {
-            state: command.$set
+            state: command.$set,
+            undo: {$set: state},
+            redo: {$set: command.$set}
         };
     }
 
     const newState = copy(state);
+    const undo = {};
+    const redo = {};
 
     if (command.hasOwnProperty('$merge')) {
         Object.assign(newState, command.$merge);
+
+        for (const property in command.$merge) {
+            Object.assign(undo, {[property]: {$set: null}});
+            Object.assign(redo, {[property]: {$set: command.$merge[property]}});
+        }
     }
 
     if (command.hasOwnProperty('$splice')) {
+        undo.$splice = [];
+        redo.$splice = command.$splice;
+
         command.$splice.forEach(args => {
-            newState.splice.apply(newState, args);
+            const removed = newState.splice.apply(newState, args);
+            undo.$splice.unshift([args[0], args.length-2].concat(removed)); //reverse order
         });
     }
 
     for (const property in command) {
         if (!commandSet[property]) {
-            newState[property] = update(state[property], command[property]).state;
+            const subResult = update(state[property], command[property]);
+            newState[property] = subResult.state;
+            undo[property] = subResult.undo;
+            redo[property] = subResult.redo;
         }
     }
 
     return {
-        state: newState
+        state: newState,
+        undo: undo,
+        redo: redo
     };
 };
 

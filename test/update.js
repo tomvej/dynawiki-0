9619fd953665298010c6c55fcdf2c5d8249c2692
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2016-04-29 15:14:08 +0200

    Another update test, babel polyfill

--- test/update.js
+++ test/update.js
@@ -1,4 +1,5 @@
 import { should } from 'chai'
+import 'babel-polyfill'
 
 import update from '../src/reducers/utils/update'
 
@@ -10,6 +11,22 @@ describe('Update Routine', function () {
             const target = {};
             update({}, {$set: target}).state
                 .should.equal(target);
+        });
+        it('should work when nested', function () {
+            update({
+                    sections: {
+                        1: {id: 1, heading: 'Section 1'},
+                        2: {id: 2, heading: 'Section 2'},
+                        3: {id: 3, heading: 'Section 3'}
+                    }
+                }, {sections: {3: {heading: {$set: 'Third Section'}}}}
+            ).state.should.deep.equal({
+                sections: {
+                    1: {id: 1, heading: 'Section 1'},
+                    2: {id: 2, heading: 'Section 2'},
+                    3: {id: 3, heading: 'Third Section'}
+                }
+            });
         });
     });
 });

30cf63574d0c4732a8b05222f450c9c6a40d5447
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2016-04-29 14:47:29 +0200

    Test rewritten to 'should' style.

--- test/update.js
+++ test/update.js
@@ -1,16 +1,15 @@
-import { expect } from 'chai'
+import { should } from 'chai'
 
 import update from '../src/reducers/utils/update'
+
+should();
 
 describe('Update Routine', function () {
     describe('$set command', function () {
         it('should work without parent', function () {
             const target = {};
-            expect(
-                update({}, {$set: target}).state
-            ).to.equal(
-                target
-            );
+            update({}, {$set: target}).state
+                .should.equal(target);
         });
     });
 });

70de9600ee2c8e2227fa8c898ac569574f85aa7c
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2016-05-01 13:27:02 +0200

    Negation helper

--- test/helpers/commands.js
+++ test/helpers/commands.js
@@ -1,5 +1,15 @@
 export default function(chai, utils) {
+    const expect = object => new chai.Assertion(object);
+    const isObject = object => {
+        expect(object).to.be.an('object');
+    };
+
+
     chai.Assertion.addProperty('command', function () {
-        
+        if (utils.flag(this, 'negate')) {
+            new chai.Assertion(() => isObject(this._obj)).to.throw(chai.AssertionError);
+        } else {
+            isObject(this._obj);
+        }
     });
 }

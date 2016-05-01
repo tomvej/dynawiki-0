92a37744dc0cfd99a1bd77bc2864c2aceac327fa
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2016-05-01 14:44:20 +0200

    Command helper tests.

--- test/helpers/commands.js
+++ test/helpers/commands.js
@@ -1,15 +1,48 @@
+const commands = {
+    $set: true,
+    $merge: true,
+    $splice: true
+};
+
 export default function(chai, utils) {
     const expect = object => new chai.Assertion(object);
-    const isObject = object => {
+    const isMergeCommand = object => {
         expect(object).to.be.an('object');
+    };
+    const isSpliceCommand = object => {
+        expect(object).to.be.an('array');
+        object.forEach(elem => {
+            expect(elem).to.be.an('array');
+            expect(elem).to.have.length.above(1);
+        });
+    };
+    const isCommand = object => {
+        expect(object).to.be.an('object');
+
+        if (object.hasOwnProperty('$merge')) {
+            isMergeCommand(object['$merge']);
+        }
+        if (object.hasOwnProperty('$splice')) {
+            isSpliceCommand(object['$splice']);
+        }
+
+        const localCommands = [];
+        Object.getOwnPropertyNames(object).forEach(property => {
+            if (commands[property]) {
+                localCommands.push(property);
+            } else {
+                isCommand(object[property]);
+            }
+        });
+        expect(localCommands).to.have.length.below(2);
     };
 
 
     chai.Assertion.addProperty('command', function () {
         if (utils.flag(this, 'negate')) {
-            new chai.Assertion(() => isObject(this._obj)).to.throw(chai.AssertionError);
+            new expect(() => isCommand(this._obj)).to.throw(chai.AssertionError);
         } else {
-            isObject(this._obj);
+            isCommand(this._obj);
         }
     });
 }
--- test/helpers/commandsTest.js
+++ test/helpers/commandsTest.js
@@ -1,4 +1,4 @@
-import chai, { should } from 'chai'
+import chai, { expect, should } from 'chai'
 
 import reducers from './commands'
 
@@ -7,27 +7,85 @@ chai.use(reducers);
 
 describe('Commands Utils', function() {
     describe('command property', function() {
+        it('should be satisfied by an empty object', function () {
+            ({}).should.be.a.command;
+        });
         it('should not be satisfied by an array', function() {
             ([]).should.not.be.a.command;
         });
+        it('should not be satisfied by a primitive', function () {
+            expect('').to.not.be.a.command;
+        });
+
         it('should be satisfied by simple $set', function () {
             ({$set: {}}).should.be.a.command;
         });
         it('should be satisfied by null $set', function () {
             ({$set: null}).should.be.a.command;
         });
+
         it('should be satisfied by simple $merge', function () {
             ({$merge: {}}).should.be.a.command;
         });
         it('should not be satisfied by null $merge', function () {
             ({$merge: null}).should.not.be.a.command;
         });
+        it('should not be satisfied by array $merge', function () {
+            ({$merge: [1,2]}).should.not.be.a.command;
+        });
+
+
         it('should not be satisfied by simple $splice', function() {
             ({$splice: {}}).should.not.be.a.command;
         });
         it('should be satisfied by empty $splice', function () {
             ({$splice: []}).should.be.a.command;
-        })
+        });
+        it('should not be satisfied by 1D $splice', function() {
+            ({$splice: [1, 2, 3]}).should.not.be.a.command;
+        });
+        it('should be satisfied by 2D $splice', function () {
+            ({$splice: [[1,2]]}).should.be.a.command;
+        });
+        it('should not be satisfied by too short $splice', function () {
+            ({$splice: [[1]]}).should.not.be.a.command;
+        });
+        it('should be satisfied by long $splice', function () {
+            ({$splice: [[1,2,5,1,6,12]]}).should.be.a.splice;
+        });
+        it('should be satisfied by multiple $splices', function () {
+            ({$splice: [[1,5], [5,1,2,3], [5,6,1]]});
+        });
+
+        it('should be satisfied by a compound command', function() {
+            ({
+                sections: {
+                    1: {$set: {id:1, heading: 'Something'}},
+                    2: {children: {$splice: [[2,3]]}}
+                },
+                editor: { $merge: {
+                    text: 'Whatever'
+                }}
+            }).should.be.a.command;
+        });
+        it('should not be satisfied by a compound command with a string property', function () {
+            ({
+                sections: {
+                    1: {$set: {id:1, heading: 'Something'}},
+                    2: {children: {$splice: [[2,3]]}},
+                    3: 'String'
+                },
+                editor: { $merge: {
+                    text: 'Whatever'
+                }}
+            }).should.not.be.a.command;
+        });
+        it('should not by satisfied by multiple command on one property', function() {
+            ({
+                $set: {},
+                $merge: {}
+            }).should.not.be.a.command;
+        });
     });
 });
 

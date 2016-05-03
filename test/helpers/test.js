771309be608ed5dacd5dd7e242f2958371a11d0b
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2016-05-03 22:29:03 +0200

    New tests for applicable helper.

--- test/helpers/test.js
+++ test/helpers/test.js
@@ -7,7 +7,7 @@ should();
 chai.use(command);
 chai.use(applicable);
 
-describe('Commands Helpers', function() {
+describe('Update Helpers', function() {
     describe('command property', function() {
         it('should accept empty object', function () {
             ({}).should.be.a.command;
@@ -134,7 +134,102 @@ describe('Commands Helpers', function() {
                 ({$set: {}}).should.applyOn([1,2,3]);
             });
         });
-        //FIXME
+        describe('$merge', function () {
+            it('should not apply to undefined', function () {
+                ({$merge: {1: {id: 1, heading: 'Section 1'}}}).should.not.applyOn(null);
+            });
+            it('should apply to empty object', function () {
+                ({$merge: {1: {id: 1, heading: 'Section 1'}}}).should.applyOn({});
+            });
+            it('should not apply to array', function () {
+                ({$merge: {1: {id: 1, heading: 'Section 1'}}}).should.not.applyOn([1, 2, 3]);
+            });
+        });
+        describe('$splice', function () {
+            it('should not apply to undefined', function () {
+                ({$splice: [[0]]}).should.not.applyOn(null);
+            });
+            it('should not apply to an object', function () {
+                ({$splice: [[0]]}).should.not.applyOn({});
+                ({$splice: [[0]]}).should.not.applyOn({sections: {
+                    1: {id: 1, heading: 'Section One'},
+                    2: {id: 2, heading: 'Section Two'}
+                }});
+            });
+            it('should apply on an array', function () {
+                ({$splice: [[0]]}).should.applyOn([]);
+            });
+            it('should not overflow', function () {
+                ({$splice: [[2]]}).should.not.applyOn([1]);
+                ({$splice: [[3,1]]}).should.not.applyOn([2,1]);
+            });
+            it('should take array size', function () {
+                ({$splice: [[2]]}).should.applyOn([2,3]);
+            });
+            it('should take negative start', function () {
+                ({$splice: [[-1]]}).should.applyOn([1, 2]);
+            });
+            it('should take negative size', function () {
+                ({$splice: [[-1]]}).should.applyOn([1]);
+            });
+            it('should not underflow', function () {
+                ({$splice: [[-2]]}).should.not.applyOn([1]);
+            });
+            it('should delete', function() {
+                ({$splice: [[0, 2]]}).should.applyOn([1,2,3]);
+            });
+            it('should delete to the end', function () {
+                ({$splice: [[1, 2]]}).should.applyOn([2,3,5]);
+            });
+            it('should not delete over end', function () {
+                ({$splice: [[1,3]]}).should.not.applyOn([2,3,1]);
+            });
+            it('should delete from negative start', function () {
+                ({$splice: [[-1, 1]]}).should.applyOn([4, 5, 6]);
+            });
+            it('should not overdelete from negative start', function () {
+                ({$splice: [[-1, 2]]}).should.not.applyOn([4,2]);
+            });
+            it('should allow adding', function () {
+                ({$splice: [[1, 0, 2, 5, 6, 3, 1]]}).should.applyOn([2,3]);
+            });
+            it('should allow various type adding', function () {
+                ({$splice: [[1, 0, 'asta', {}, []]]}).should.applyOn([2, 3]);
+            });
+            it('should allow adding while deleting', function () {
+                ({$splice: [[1, 3, 5, 3]]}).should.applyOn([2,3,4,5]);
+            });
+            it('should work with multiple adds and deletes', function () {
+                ({$splice: [[-2, 1], [0, 1, 2, 3, 4], [1, 1]]}).should.applyOn([4, 3, 2]);
+            });
+            it('should not accept multiple adds and deletes which do not add up', function () {
+                ({$splice: [[1, 1], [2, 1, 1], [1, 2], [1, 1]]}).should.not.applyOn([3, 4, 5, 6]);
+            });
+        });
+        it('should apply on substructure', function () {
+            ({sections: {
+                1: {$set: {id: 2, heading: 'What do you know'}},
+                5: {children: {$splice: [[1, 0, 4, 3]]}},
+                $merge: {
+                    3: {id: 3, heading: 'Third section'},
+                    4: {id: 4, heading: 'Fourth section'}
+                }
+            }}).should.applyOn({sections: {
+                1: {id: 1, heading: 'First section'},
+                2: {id: 2, heading: 'Second section'},
+                5: {id: 5, children: [4, 5]},
+                6: {id: 6, children: [10, 12]}
+            }});
+        });
+        it('should not apply on missing links', function () {
+            ({
+                1: {id: {$set: 2}},
+                2: {$set: {id: 3}}
+            }).should.not.applyOn({
+                1: {heading: 'First section'},
+                2: {id: 5, heading: 'Second section'}
+            });
+        });
     })
 });
 

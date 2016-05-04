e715cebf3324a889054573540f13468f123ec90e
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2016-05-04 21:52:02 +0200

    Update $merge tests

--- test/update.js
+++ test/update.js
@@ -29,4 +29,34 @@ describe('Update Routine', function () {
             });
         });
     });
+    describe('merge command', function() {
+        it('should work without parent', function () {
+            update({}, {$merge: {1: {id: 1, heading: 'Section One'}}}).state
+                .should.deep.equal({1: {id: 1, heading: 'Section One'}});
+        });
+        it('should work when nested', function () {
+            update({
+                sections: {
+                    1: {id: 1, heading: 'Section 1'},
+                    2: {id: 2, heading: 'Section 2'}
+                }
+            }, {sections: {2: {$merge: {children: [1,2,3]}}}}).state
+                .should.deep.equal({
+                sections: {
+                    1: {id: 1, heading: 'Section 1'},
+                    2: {id: 2, heading: 'Section 2', children: [1,2,3]}
+                }
+            });
+        });
+        it('should overwrite existing elements', function () {
+            update({
+                1: {id: 1, heading: 'Section One'},
+                2: {id: 2, heading: 'Section Two'}
+            }, {$merge: {2: {id: 3, heading: 'Section Three'}}}).state
+                .should.deep.equal({
+                1: {id: 1, heading: 'Section One'},
+                2: {id: 3, heading: 'Section Three'}
+            });
+        });
+    });
 });

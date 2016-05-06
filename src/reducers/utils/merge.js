f8099b2e9fd8c59b75d67dddf93b8a090bcff795
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2016-05-06 14:32:49 +0200

    Merge rewritten to match tests.

--- src/reducers/utils/merge.js
+++ src/reducers/utils/merge.js
@@ -1,19 +1,19 @@
 const isOtherObject = object => (!Array.isArray(object) && typeof object === 'object');
 
 const merge = (target, object) => {
-    Object.getOwnPropertyNames(object).forEach(property => {
-        if (target.hasOwnProperty(property)) {
-            if (Array.isArray(target[property]) && Array.isArray(object[property])) {
-                [].push.apply(target[property], object[property]);
-            } else if (isOtherObject(target[property]) && isOtherObject(object[property])) {
+    if(Array.isArray(target) && Array.isArray(object)) {
+        [].push.apply(target, object);
+    } else if (isOtherObject(target) && isOtherObject(object)) {
+        Object.getOwnPropertyNames(object).forEach(property => {
+            if (target.hasOwnProperty(property)) {
                 merge(target[property], object[property]);
             } else {
-                throw 'Cannot merge ' + target[property] + ' and ' + object[property];
+                target[property] = object[property];
             }
-        } else {
-            target[property] = object[property];
-        }
-    });
+        });
+    } else {
+        throw `Cannot merge ${target} and ${object}.`;
+    }
 };
 
 export default merge;

cbccd28fab6f8f4204d918261d4e3e5ac3316d17
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2016-05-06 19:22:06 +0200

    Publish reducer with helpers and simple test.

---
+++ test/publishReducer.js
@@ -0,0 +1,77 @@
+import chai from 'chai'
+import 'babel-polyfill'
+
+import publish from '../src/reducers/publish'
+import update from '../src/reducers/utils/update'
+
+chai.should();
+
+
+const state = object => {
+    const nextId = object.nextId;
+    const editor = {
+        section: object.section,
+        index: object.index,
+        text: ''
+    };
+    delete object.nextId;
+    delete object.section;
+    delete object.index;
+    return {
+        sections: object,
+        nextId: nextId,
+        editor: editor
+    };
+};
+
+chai.use(function (chai, utils) {
+    chai.Assertion.addMethod('update', function (object) {
+        utils.flag(this, 'source', object);
+    });
+    chai.Assertion.addChainableMethod('to', function (object) {
+        new chai.Assertion(this._obj).to.be.an('array');
+        const source = state(utils.flag(this, 'source'));
+        update(source, publish(source, this._obj)).state.should.deep.equal(state(object));
+    }, function () {});
+});
+
+const paragraph = text => ({
+    type: 'paragraph',
+    text: text
+});
+const section = (level, heading) => ({
+    type: 'section',
+    heading: heading,
+    level: level
+});
+
+describe('Publish Reducer', function () {
+    it('should append paragraph', function () {
+        [
+            paragraph('Paragraph')
+        ].should.update({
+            nextId: 1,
+            section: 0,
+            index: 0,
+            0: {
+                id: 0,
+                heading: 'H1',
+                contents: [],
+                children: []
+            }
+        }).to({
+            nextId: 2,
+            section: 0,
+            index: 1,
+            0: {
+                id: 0,
+                heading: 'H1',
+                contents: [{
+                    id: 1,
+                    text: 'Paragraph'
+                }],
+                children: []
+            }
+        });
+    });
+});

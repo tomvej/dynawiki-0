c97881eeb48b287025cd2f4eb2161b8127467943
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2016-05-06 14:54:37 +0200

    Immutable merge rewritten to satisfy tests.

--- src/reducers/utils/mergeImmutable.js
+++ src/reducers/utils/mergeImmutable.js
@@ -1,30 +1,26 @@
 const isObject = target => !Array.isArray(target) && typeof target === 'object';
 
 const merge = (object, target) => {
-    const mergeProperty = (object, target) => {
-        if (Array.isArray(object) && Array.isArray(target)) {
-            return object.concat(target);
-        } else if (isObject(object) && isObject(target)) {
-            return merge(object, target);
-        } else {
-            throw 'Cannot merge ' + object + ' and ' + target;
-        }
-    };
-
-    const result = {};
-    Object.getOwnPropertyNames(object).forEach(property => {
-        if (target.hasOwnProperty(property)) {
-            result[property] = mergeProperty(object[property], target[property]);
-        } else {
-            result[property] = object[property];
-        }
-    });
-    Object.getOwnPropertyNames(target).forEach(property => {
-        if (!object.hasOwnProperty(property)) {
-            result[property] = target[property];
-        }
-    });
-    return result;
+    if (Array.isArray(object) && Array.isArray(target)) {
+        return object.concat(target);
+    } else if (isObject(object) && isObject(target)) {
+        const result = {};
+        Object.getOwnPropertyNames(object).forEach(property => {
+            if(target.hasOwnProperty(property)) {
+                result[property] = merge(object[property], target[property]);
+            } else {
+                result[property] = object[property];
+            }
+        });
+        Object.getOwnPropertyNames(target).forEach(property => {
+            if (!object.hasOwnProperty(property)) {
+                result[property] = target[property];
+            }
+        });
+        return result;
+    } else {
+        throw `Cannot merge ${object} and ${target}.`;
+    }
 };
 
 export default merge;

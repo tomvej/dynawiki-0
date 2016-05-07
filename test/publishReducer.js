f5145babd833888f44eada4848d702adebc65221
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2016-05-07 16:50:25 +0200

    Subsection insertion tests

--- test/publishReducer.js
+++ test/publishReducer.js
@@ -6,7 +6,6 @@ import update from '../src/reducers/utils/update'
 
 chai.should();
 
-
 const state = object => {
     const nextId = object.nextId;
     const editor = {
@@ -104,6 +103,155 @@ describe('Publish Reducer', function () {
                 ],
                 children: []
             }
+        });
+    });
+    it('should append lower-level section', function () {
+        [
+            section(0, 'Subsection')
+        ].should.update({
+            nextId: 1,
+            section: 0,
+            index: 0,
+            0: {
+                id: 0,
+                heading: 'Section',
+                children: [],
+                contents: []
+            }
+        }).to({
+            nextId: 2,
+            section: 1,
+            index: 0,
+            0: {
+                id: 0,
+                heading: 'Section',
+                children: [1],
+                contents: []
+            },
+            1: {
+                id: 1,
+                parent: 0,
+                heading: 'Subsection',
+                children: [],
+                contents: []
+            }
+        });
+    });
+    it('should append lower-level section with paragraph', function () {
+        [
+            section(0, 'Subsection'),
+            paragraph('Subsection paragraph')
+        ].should.update({
+            nextId: 2,
+            section: 0,
+            index: 1,
+            0: {
+                id: 0,
+                heading: 'Section',
+                children: [],
+                contents: [{
+                    id: 1,
+                    text: 'Paragraph'
+                }]
+            }
+        }).to({
+            nextId: 4,
+            section: 2,
+            index: 1,
+            0: {
+                id: 0,
+                heading: 'Section',
+                children: [2],
+                contents: [{
+                    id: 1,
+                    text: 'Paragraph'
+                }]
+            },
+            2: {
+                id: 2,
+                parent: 0,
+                heading: 'Subsection',
+                children: [],
+                contents: [{
+                    id: 3,
+                    text: 'Subsection paragraph'
+                }]
+            }
+        });
+    });
+    it('should insert lower-level section', function() {
+        [
+            section(0, 'Another subsection')
+        ].should.update({
+            0: {
+                id: 0,
+                heading: 'Main section',
+                children: [],
+                contents: [
+                    {id: 1, text: 'First paragraph'},
+                    {id: 2, text: 'Second paragraph'}
+                ]
+            },
+            nextId: 3,
+            section: 0,
+            index: 1
+        }).to({
+            0: {
+                id: 0,
+                heading: 'Main section',
+                children: [3],
+                contents: [{id: 1, text: 'First paragraph'}]
+            },
+            3: {
+                id: 3,
+                parent: 0,
+                heading: 'Another subsection',
+                children: [],
+                contents: [{id: 2, text: 'Second paragraph'}]
+            },
+            nextId: 4,
+            section: 3,
+            index: 0
+        });
+    });
+    it('should insert lower-level section with paragraph', function () {
+        [
+            section(0, 'Just a Section'),
+            paragraph('Some kind of a paragraph')
+        ].should.update({
+            0: {
+                id: 0,
+                heading: 'New page',
+                children: [],
+                contents: [
+                    {id: 1, text: 'First paragraph'},
+                    {id: 2, text: 'Second paragraph'}
+                ]
+            },
+            nextId: 3,
+            section: 0,
+            index: 1
+        }).to({
+            0: {
+                id: 0,
+                heading: 'New page',
+                children: [3],
+                contents: [{id: 1, text: 'First paragraph'}]
+            },
+            3: {
+                id: 3,
+                parent: 0,
+                heading: 'Just a Section',
+                children: [],
+                contents: [
+                    {id: 4, text: 'Some kind of a paragraph'},
+                    {id: 2, text: 'Second paragraph'}
+                ]
+
+            },
+            nextId: 5,
+            section: 3,
+            index: 1
         });
     });
 });

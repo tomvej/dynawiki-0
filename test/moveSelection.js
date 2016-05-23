166aade867db92dbccfddaec71eee3acec8bea87
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2016-05-23 14:03:41 +0200

    Tests rewritten using helpers.

--- test/moveSelection.js
+++ test/moveSelection.js
@@ -62,15 +62,13 @@ describe('Move Selection Reducer', function () {
             });
         });
         it('should move from section to parent section', function () {
-            move(state({
+            Direction.PARENT.should.change(1, null).into(0, null).in({
                 0: {id: 0, parent: null, contents: [1]},
-                1: {id: 1, parent: 0},
-                section: 1,
-                index: null
-            }), Direction.PARENT).should.deep.equal({selection: {section: {$set: 0}}});
+                1: {id: 1, parent: 0}
+            });
         });
         it('should move from section to parent section even with preceding paragraphs and sections', function () {
-            move(state({
+            Direction.PARENT.should.change(2, null).into(0, null).in({
                 0: {
                     id: 0, parent: null, contents: [1, 2], children: [
                         {id: 3},
@@ -83,19 +81,15 @@ describe('Move Selection Reducer', function () {
                         {id: 6}
                     ]
                 },
-                2: {id: 2, parent: 0},
-                section: 2,
-                index: null
-            }), Direction.PARENT).should.deep.equal({selection: {section: {$set: 0}}});
+                2: {id: 2, parent: 0}
+            });
         });
     });
     describe('move up', function () {
         it('should not move from first section', function () {
-            void expect(move(state({
+            Direction.UP.should.not.change(0, null).in({
                 0: {id: 0, parent: null},
-                section: 0,
-                index: null
-            }), Direction.UP)).to.be.null;
+            });
         });
         it('should move to previous paragraph in a seciton');
         it('should move from first paragraph to parent section');
@@ -109,105 +103,87 @@ describe('Move Selection Reducer', function () {
     });
     describe('move down', function () {
         it('should move to next paragraph in a section', function () {
-            move(state({
+            Direction.DOWN.should.change(0, 1).into(0, 2).in({
                 0: {
                     id: 0, parent: null, contents: [
                         {id: 1},
                         {id: 2},
                         {id: 3}
                     ]
-                },
-                section: 0,
-                index: 1
-            }), Direction.DOWN).should.deep.equal({selection: {index: {$set: 2}}});
+                }
+            });
         });
         it('should move from section to its first paragraph', function () {
-            move(state({
+            Direction.DOWN.should.change(0, null).into(0, 0).in({
                 0: {
                     id: 0, parent: null, contents: [
                         {id: 1},
                         {id: 2}
                     ]
-                },
-                section: 0,
-                index: null
-            }), Direction.DOWN).should.deep.equal({selection: {index: {$set: 0}}});
+                }
+            });
         });
         it('should move from last paragraph to first child section', function () {
-            move(state({
+            Direction.DOWN.should.change(0, 1).into(2, null).in({
                 0: {id: 0, parent: null, contents: [{id: 1}], children: [2]},
-                2: {id: 2, parent: 0},
-                section: 0,
-                index: 1
-            }), Direction.DOWN).should.deep.equal({selection: {$set: {section: 2, index: null}}});
+                2: {id: 2, parent: 0}
+            });
         });
         it('should move from paragraph-less section to to first child section', function () {
-            move(state({
+            Direction.DOWN.should.change(0, null).into(1, null).in({
                 0: {id: 0, parent: null, children: [1]},
-                1: {id: 1, parent: 0},
-                section: 0,
-                index: null
-            }), Direction.DOWN).should.deep.equal({selection: {section: {$set: 1}}});
+                1: {id: 1, parent: 0}
+            });
         });
         it('should move from last paragraph to next section', function () {
-            move(state({
+            Direction.DOWN.should.change(1, 0).into(3, null).in({
                 0: {id: 0, parent: null, children: [1, 3]},
                 1: {id: 1, parent: 0, contents: [{id: 2}]},
-                3: {id: 3, parent: 0},
-                section: 1,
-                index: 0
-            }), Direction.DOWN).should.deep.equal({selection: {$set: {section: 3, index: null}}});
+                3: {id: 3, parent: 0}
+            });
         });
         it('should move from last subsubsection to next section', function () {
-            move(state({
+            Direction.DOWN.should.change(4, null).into(2, null).in({
                 0: {id: 0, parent: null, children: [1, 2]},
                 1: {id: 1, parent: 0, children: [3]},
                 2: {id: 2, parent: 0},
                 3: {id: 3, parent: 1, children: [4]},
-                4: {id: 4, parent: 3},
-                section: 4,
-                index: null
-            }), Direction.DOWN).should.deep.equal({selection: {section: {$set: 2}}});
+                4: {id: 4, parent: 3}
+            });
         });
         it('should move from last subsubsection paragraph to next section', function () {
-            move(state({
+            Direction.DOWN.should.change(4, 0).into(2, null).in({
                 0: {id: 0, parent: null, children: [1, 2]},
                 1: {id: 1, parent: 0, children: [3]},
                 2: {id: 2, parent: 0},
                 3: {id: 3, parent: 1, children: [4]},
-                4: {id: 4, parent: 3, contents: [{id: 5}]},
-                section: 4,
-                index: 0
-            }), Direction.DOWN).should.deep.equal({selection: {$set: {section: 2, index: null}}});
+                4: {id: 4, parent: 3, contents: [{id: 5}]}
+            });
         });
         it('should not move from last paragraph without subsection', function () {
-            void expect(move(state({
+            Direction.DOWN.should.not.change(0, 2).in({
                 0: {
                     id: 0, parent: null, contents: [
                         {id: 1},
                         {id: 2},
                         {id: 3}
                     ]
-                },
-                section: 0,
-                index: 2
-            }), Direction.DOWN)).to.be.null;
+                }
+            });
         });
         it('should not move from last subsection', function () {
-            void expect(move(state({
+            Direction.DOWN.should.not.change(1, null).in({
                 0: {id: 0, parent: null, children: [1]},
                 1: {id: 1, parent: 0},
                 section: 1,
                 index: null
-            }), Direction.DOWN)).to.be.null;
+            });
         });
         it('should not move from last paragraph of subsection', function () {
-            void expect(move(state({
+            Direction.DOWN.should.not.change(1, 1).in({
                 0: {id: 0, parent: null, children: [1]},
-                1: {id: 1, parent: 0, contents: [{id: 2}]},
-                section: 1,
-                index: 1
-            }), Direction.DOWN)).to.be.null;
+                1: {id: 1, parent: 0, contents: [{id: 2}]}
+            });
         });
     });
 });

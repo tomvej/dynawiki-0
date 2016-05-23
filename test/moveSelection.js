dc45b22e9e9c6a6645e6201080c4dbbe0a83ff1e
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2016-05-23 09:42:26 +0200

    Move selection down tests.

--- test/moveSelection.js
+++ test/moveSelection.js
@@ -95,6 +95,85 @@ describe('Move Selection Reducer', function () {
 
     });
     describe('move down', function () {
-
+        it('selects next paragraph in a section', function () {
+            move({
+                sections: {
+                    0: {
+                        id: 0, parent: null, contents: [
+                            {id: 1},
+                            {id: 2},
+                            {id: 3}
+                        ]
+                    }},
+                selection: {section: 0, index: 1}
+            }, Direction.DOWN).should.deep.equal({selection: {index: {$set: 2}}});
+        });
+        it('selects first paragraph from section', function () {
+            move({
+                sections: {
+                    0: {
+                        id: 0, parent: null, contents: [
+                            {id: 1},
+                            {id: 2}
+                        ]
+                    }
+                },
+                selection: {section: 0, index: null}
+            }, Direction.DOWN).should.deep.equal({selection: {index: {$set: 0}}});
+        });
+        it('selects first child section from last paragraph', function () {
+            move({
+                sections: {
+                    0: {id: 0, parent: null, contents: [{id: 1}], children: [2]},
+                    2: {id: 2, parent: 0}
+                },
+                selection: {section: 0, index: 1}
+            }, Direction.DOWN).should.deep.equal({selection: {$set: {section: 2, index: null}}});
+        });
+        it('selects first child section from paragraph-less section', function () {
+            move({
+                sections: {
+                    0: {id: 0, parent: null, children: [1]},
+                    1: {id: 1, parent: 0}
+                },
+                selection: {section: 0, index: null}
+            }, Direction.DOWN).should.deep.equal({selection: {section: {$set: 1}}});
+        });
+        it('selects next section from last paragraph', function () {
+            move({
+                sections: {
+                    0: {id: 0, parent: null, children: [1, 3]},
+                    1: {id: 1, parent: 0, contents: [{id: 2}]},
+                    3: {id: 3, parent: 0}
+                },
+                selection: {section: 1, index: 0}
+            }, Direction.DOWN).should.deep.equal({selection: {$set: {section: 3, index: null}}});
+        });
+        it('selects next section from last subsubsection', function () {
+            move({
+                sections: {
+                    0: {id: 0, parent: null, children: [1, 2]},
+                    1: {id: 1, parent: 0, children: [3]},
+                    2: {id: 2, parent: 0},
+                    3: {id: 3, parent: 1, children: [4]},
+                    4: {id: 4, parent: 3}
+                },
+                selection: {section: 4, index: null}
+            }, Direction.DOWN).should.deep.equal({selection: {section: {$set: 2}}});
+        });
+        it('selects next section from last subsubsection paragraph', function () {
+            move({
+                sections: {
+                    sections: {
+                        0: {id: 0, parent: null, children: [1, 2]},
+                        1: {id: 1, parent: 0, children: [3]},
+                        2: {id: 2, parent: 0},
+                        3: {id: 3, parent: 1, children: [4]},
+                        4: {id: 4, parent: 3, contents: [{id: 5}]}
+                    }
+                },
+                selection: {section: 4, index: 0}
+            }, Direction.DOWN).should.deep.equal({selection: {$set: {section: 2, index: null}}});
+        });
     });
 });

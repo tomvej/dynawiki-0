92f41347fb9cd54ccc6be6ab0a6340916de7bc89
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2016-06-17 21:59:12 +0200

    Context menu key

--- src/containers/Page.js
+++ src/containers/Page.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 import OutsideClickWrapper from '../components/OutsideClickWrapper'
-import { clearSelection, startSelection } from '../actions'
+import { clearSelection, startSelection, popup } from '../actions'
 import { moveSelectionDown, moveSelectionUp, moveSelectionToParent } from '../actions/moveSelection'
 
 const handleKeyPress = dispatch => event => {
@@ -27,15 +27,24 @@ const handleKeyPress = dispatch => event => {
 
 const mapDispatchToProps = dispatch => ({
     clearSelection: () => dispatch(clearSelection()),
-    onKeyPress: handleKeyPress(dispatch)
+    onKeyPress: handleKeyPress(dispatch),
+    contextMenu: event => {
+        event.preventDefault();
+        if (event.buttons === 0) {
+            dispatch(popup(true));
+        }
+
+    }
 });
 
 class Page extends React.Component {
     componentDidMount() {
         window.addEventListener('keypress', this.props.onKeyPress, false);
+        window.addEventListener('contextmenu', this.props.contextMenu);
     }
     componentWillUnmount() {
         window.removeEventListener('keypress', this.props.onKeyPress, false);
+        window.removeEventListener('contextnenu', this.props.contextMenu);
     }
     render() {
         return <OutsideClickWrapper onOutsideClick={this.props.clearSelection}>
